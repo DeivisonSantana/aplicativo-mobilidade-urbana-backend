08b3b7391248e5632300f5138aeffab6c491f27f
diogo-guimaraes <diogoguimaraes.br@hotmail.com>
2026-04-22 18:04:58 -0400

    versão com foto funcional

--- app/Http/Controllers/API/UsuarioController.php
+++ app/Http/Controllers/API/UsuarioController.php
@@ -5,9 +5,10 @@ namespace App\Http\Controllers\API;
 use App\Enums\UserType;
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use GrahamCampbell\ResultType\Success;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Auth;
+use Intervention\Image\Laravel\Facades\Image;
 
 class UsuarioController extends Controller
 {
@@ -36,8 +37,6 @@ class UsuarioController extends Controller
      */
     public function store(Request $request)
     {
-        $input = $request->all();
-
         $user = User::create([
             "name" => $request->name,
             "data_nascimento" => $request->data_nascimento,
@@ -47,14 +46,47 @@ class UsuarioController extends Controller
             "email" => $request->email,
             "foto" => $request->foto,
             "status" => 'aprovação',
-            "password" => bcrypt($input['password'])
+            "password" => bcrypt($request->password)
         ]);
+
+        $image = $request->file('image', null);
+
+        if ($image) {
+            $host = $this->SchemeAndHttpHost($request);
+            $request->validate([
+                'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg',
+            ]);
+
+
+            $image = $request->file('image');
+            $imageName = $image->getClientOriginalName();
+            $imageName = time() . '_' . $imageName;
+            $thumbnail = $image->getClientOriginalName();
+            $thumbnail = time() . '_thumbnail' . $thumbnail;
+
+            // Versão 4.0 usando o facade
+            Image::decode($image)
+                ->resize(100, 100)
+                ->save(public_path('images/') . $thumbnail);
+
+            $image->move(public_path('images'), $imageName);
+            $user->foto = "{$host}/images/{$thumbnail}";
+            $user->saveOrFail();
+        }
 
         return response()->json([
             'success' => true,
             'message' => 'Registro realizado com sucesso',
             'data' => $user
         ], 201);
+    }
+
+    public function SchemeAndHttpHost($request)
+    {
+        if (App::environment('local')) {
+            return $request->getSchemeAndHttpHost();
+        }
+        return "https://api.pen6.app/";
     }
 
     /**
