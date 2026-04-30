4676b130358f9a109e395a4c14c6c78f8dacfcec
diogo-guimaraes <diogoguimaraes.br@hotmail.com>
2026-04-30 15:17:09 -0400

    add Passageiro

---
+++ app/Http/Controllers/PassageiroController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Passageiro;
+use Illuminate\Http\Request;
+
+class PassageiroController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Passageiro $passageiro)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Passageiro $passageiro)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Passageiro $passageiro)
+    {
+        //
+    }
+}
